3cf27d538e44b28bb44d7ad84d5a70d9aba02908
Razvan Mazilu <razvanc.mazilu@gmail.com>
2023-12-04 00:18:42 +0200

    add orizontal drawing lines, and down left corner

--- src/main.cpp
+++ src/main.cpp
@@ -89,9 +89,9 @@ public:
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                            {0, 0, 0, 0, 0, 0, 0, 2, 3, 0, 3, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                            {0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -104,12 +104,49 @@ public:
     };
     void Draw() 
     {
-        for (int i = 0; i < cellCount; i++)
+        for (int i = 0; i < cellCount; i++) {
             for(int j = 0; j < cellCount; j++) {
                 if (mazeArr[i][j] == 1) {
-                    DrawRectangle(offset + j * cellSize, offset + i * cellSize, cellSize, cellSize, darkBlue);
+
+                    // draw 2 vertical lines for each 1 at limit + 2 and limit + 18
+                    Vector2 start = {offset + 2 + static_cast<float>(cellSize * j), offset + static_cast<float>(cellSize * i)};
+                    Vector2 end = {offset + 2 + static_cast<float>(cellSize * (j)), offset + static_cast<float>(cellSize * (i +1)) };
+                    DrawLineEx(start, end, 2, darkBlue);
+                    start = {offset + 18 + static_cast<float>(cellSize * j), offset + static_cast<float>(cellSize * i)};
+                    end = {offset + 18 + static_cast<float>(cellSize * (j)), offset + static_cast<float>(cellSize * (i +1)) };
+                    //DrawRectangle(offset + j * cellSize, offset + i * cellSize, cellSize, cellSize, darkBlue);
+                    DrawLineEx(start, end, 2, darkBlue);
+                
+                } else if (mazeArr[i][j] == 5) {
+                        // draw 2 orizontal lines for each 1 at limit + 2 and limit + 18
+                    Vector2 start = {offset + static_cast<float>(cellSize * j), offset + 2 +  static_cast<float>(cellSize * i)};
+                    Vector2 end = {offset + static_cast<float>(cellSize * (j + 1)), offset + 2 + static_cast<float>(cellSize * (i)) };
+                    DrawLineEx(start, end, 2, darkBlue);
+                    start = {offset + static_cast<float>(cellSize * j), offset + 18 + static_cast<float>(cellSize * i)};
+                    end = {offset + static_cast<float>(cellSize * (j + 1)), offset +  18 + static_cast<float>(cellSize * (i)) };
+                    //DrawRectangle(offset + j * cellSize, offset + i * cellSize, cellSize, cellSize, darkBlue);
+                    DrawLineEx(start, end, 2, darkBlue);
+                } 
+                
+                else if (mazeArr[i][j] == 4) {
+                    // draw down left corner
+                    Rectangle rec = { (offset + 1 + static_cast<float>(cellSize * j)), ((offset + 1 + static_cast<float>(cellSize * i))), static_cast<float>(cellSize - 2), static_cast<float>(cellSize - 2)};
+                    DrawRectangleLinesEx(rec, 2, darkBlue); 
+                    Vector2 start = {(offset + 1 + static_cast<float>(cellSize * j)),(offset + 2 + static_cast<float>(cellSize * i))};
+                    Vector2 end;
+                    end.x = start.x + static_cast<float>(cellSize - 2);
+                    end.y = start.y;
+                    DrawLineEx(start, end, 2, black);   
+                    start.x = offset - 2  + static_cast<float>(cellSize * (j + 1));
+                    start.y = offset + 1 + static_cast<float>(cellSize * (i));
+                    end.x = start.x;
+                    end.y = start.y + static_cast<float>(cellSize - 2);
+                    DrawLineEx(start, end, 2, black);  
                 }
+
             }
+        }
+                
     }
 };
 
